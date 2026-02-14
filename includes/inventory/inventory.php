a997dcf7d28e41dffb1aa4135a876ef1643c66b4
Reza Mohammadzadeh <rezamz.dev@gmail.com>
2026-02-14 15:29:27 +0330

    loading files in front-end

--- includes/inventory/inventory.php
+++ includes/inventory/inventory.php
@@ -1,19 +1,12 @@
 <?php
 defined('ABSPATH') || exit;
 
-//add_action('wp_enqueue_scripts', 'rm_inventory_scripts');
-require_once __DIR__ . '/products-query.php';
-require_once __DIR__ . '/products-edit.php';
-require_once __DIR__ . '/products-ajax.php';
+if (!is_admin()) {
 
-//function rm_inventory_scripts()
-//{
-//    wp_enqueue_script(
-//        'rm-inventory',
-//        RM_URL . 'includes/modules/inventory/inventory.js', [], RM_VER, true);
-//
-//    wp_localize_script('rm-inventory', 'rm_inventory_ajax', [
-//        'ajax_url' => admin_url('admin-ajax.php'),
-//        'nonce' => wp_create_nonce('RM_UPDATE_INVENTORY'),
-//    ]);
-//}
+    if (is_page(4589)) {
+        require_once __DIR__ . '/products-query.php';
+        require_once __DIR__ . '/products-edit.php';
+        require_once __DIR__ . '/products-ajax.php';
+    }
+
+}
